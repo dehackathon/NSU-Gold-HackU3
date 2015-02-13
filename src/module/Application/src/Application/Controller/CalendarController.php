3a276719760c4cb29ca409cd5200dd04c75e5f12
tswayne <tyler.swayne@dominionmarinemedia.com>
2015-02-13 10:30:18 -0500

    add stubbed events

--- src/module/Application/src/Application/Controller/CalendarController.php
+++ src/module/Application/src/Application/Controller/CalendarController.php
@@ -40,19 +40,56 @@ class CalendarController extends AbstractActionController
 
     public function eventsAction()
     {
-        $data = array(
+        $data = $this->getMockedEvents();
+        return new JsonModel($data);
+    }
+
+    private function getMockedEvents()
+    {
+        return array(
             'success' => 1,
             'result' => array(
                 array(
                     'id' => 2,
+                    'title' => 'Hackathon: 150 Granby Street, Norfolk, VA 23510',
+                    'url' => 'http://hackathon.dominionenterprises.com/',
+                    'class' => 'event-important',
+                    'start' => 1423832400000,
+                    'end' => 1423864800000
+                ),
+                array(
+                    'id' => 3,
+                    'title' => 'Party!',
+                    'url' => 'https://www.google.com/maps/place/Field+Guide/@36.852047,-76.289591,17z/data=!3m1!4b1!4m2!3m1!1s0x89ba981211ff40c9:0x192b179c66f54eda',
+                    'class' => 'event-info',
+                    'start' => 1423875600000,
+                    'end' => 1423889880000
+                ),
+                array(
+                    'id' => 4,
                     'title' => 'Event 1',
                     'url' => 'http://example.com',
-                    'class' => 'event-important',
-                    'start' => 1423803600000,
-                    'end' => 1423803600100
+                    'class' => 'event-special',
+                    'start' => 1423845900000,
+                    'end' => 1423853100000
+                ),
+                array(
+                    'id' => 5,
+                    'title' => 'Event 1',
+                    'url' => 'http://example.com',
+                    'class' => 'event-success',
+                    'start' => 1423845900000,
+                    'end' => 1423853100000
+                ),
+                array(
+                    'id' => 6,
+                    'title' => 'Event 1',
+                    'url' => 'http://example.com',
+                    'class' => 'event-warning',
+                    'start' => 1423845900000,
+                    'end' => 1423853100000
                 )
             )
         );
-        return new JsonModel($data);
     }
 }

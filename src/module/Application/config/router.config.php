6ddd4ed4dc79cd794900fae9d2a255d1cb258e0f
Singlee JP Murray <singleemurray79@gmail.com>
2015-02-13 09:23:44 -0500

    add router/controller for user-list page

--- src/module/Application/config/router.config.php
+++ src/module/Application/config/router.config.php
@@ -94,6 +94,16 @@ return array(
             ),
 
         ),
+        'user-list' => array(
+            'type' => 'Zend\Mvc\Router\Http\Literal',
+            'options' => array(
+                'route'    => '/user-list',
+                'defaults' => array(
+                    'controller' => 'Application\Controller\UserList',
+                    'action'     => 'index',
+                ),
+            ),
+        ),
         // The following is a route to simplify getting started creating
         // new controllers and actions without needing to create a new
         // module. Simply drop new controllers in, and you can access them
